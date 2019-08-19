808cbbb4d4e4814bf1a9df2fce5d5f2e8bc9511a
melissa asiamah <melissatasiamah@gmail.com>
2019-08-19 12:29:03 -0400

    new commit

--- server.js
+++ server.js
@@ -6,9 +6,7 @@
 //CONFIG FILE IS FOR STUFF NOT PUSHING TO GITHUB
 // get all the tools we need
 
-if (process.env.NODE_ENV !== 'production'){
 require('dotenv').config({silent: true});
-}
 var express  = require('express');
 var app      = express();
 var port     = process.env.PORT || 4000;
