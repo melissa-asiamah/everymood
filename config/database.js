42c624159467f7070177bdb7fe08178c87621781
melissa asiamah <melissatasiamah@gmail.com>
2019-08-19 13:36:04 -0400

    new commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : `mongodb+srv://melissa91:$ilovemongodb1@cluster0-xu4rv.mongodb.net/Everymood?retryWrites=true&w=majority`, // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb+srv://melissa91:ilovemongodb1@cluster0-xu4rv.mongodb.net/Everymood?retryWrites=true&w=majority', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
     'dbName': 'final-project'
 };
